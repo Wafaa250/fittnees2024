243f16bca94bba9d74778dcdc98dcb42b7303eeb
Wafaa250 <wafaaabosamra78@gmail.com>
2025-01-03 18:41:52 +0200

    Update AccepTest.java

--- src/test/java/fit/AccepTest.java
+++ src/test/java/fit/AccepTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-	    features = "C:\\Users\\Msys\\eclipse-workspace\\fit\\src\\feature", 
+    features = "file:///C:/Users/Msys/eclipse-workspace/fit/src/feature",
 	    plugin = { "pretty", "html:target/cucumber/wikipedia.html" }, 
 	    monochrome = true, 
 	    snippets = SnippetType.CAMELCASE, 
